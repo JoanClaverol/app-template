19faa8117853075fd559bff4fd5fb0be26c1bd94
JoanClaverol <41630699+JoanClaverol@users.noreply.github.com>
2023-02-09 21:12:40 +0100

    almost finished, missing the image export

--- src/App.js
+++ src/App.js
@@ -6,16 +6,57 @@ import { useState } from "react";
 function App() {
 
   const { data, loading, error } = useFetchData("https://api.imgflip.com/get_memes");
-
   const [memeDisplayed, setMemeDisplayed] = useState(null);
   const shuffleMemes = () => {
     // get a random element from data.data.memes[0]
-    setMemeDisplayed(data.data.memes[Math.floor(Math.random() * data.data.memes.length)]);
+    setMemeDisplayed(
+      // create an element that contains a url, a height and a width
+      {
+        url: data.data.memes[Math.floor(Math.random() * data.data.memes.length)].url,
+        height: data.data.memes[Math.floor(Math.random() * data.data.memes.length)].height,
+        width: data.data.memes[Math.floor(Math.random() * data.data.memes.length)].width,
+      }
+    );
+  }
+
+  const handleFileUpload = (e) => {
+    // set the meme displayed
+    setMemeDisplayed(
+      {
+        url: URL.createObjectURL(e.target.files[0]),
+        // find a way to get the height and width of the image
+        height: 500,
+        width: 500,
+      }
+    );
+    e.target.value = null;
   }
 
   if (!loading && data.data.memes.length > 0 && !memeDisplayed) {
     shuffleMemes();
   }
+
+  const buttonStyle = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    // add top margin
+    marginTop: '20px',
+    // add marginright and left
+    margin: '0 5px',
+    // add padding
+    padding: '5px',
+    // add border
+    border: 'none',
+    // add border radius
+    borderRadius: '5px',
+    // add background color
+    backgroundColor: 'lightblue',
+    // add cursor
+    cursor: 'pointer',
+  };
+
 
   return (
     <div className="App">
@@ -23,34 +64,28 @@ function App() {
         <p>Error: {error.message}</p> :
         memeDisplayed ?
           <>
-            <h1 style={{ textAlign: "center" }}>Meme Creator</h1>
-            <button
-              onClick={shuffleMemes}
+            <h1 style={{
+              textAlign: "center",
+              // color white
+              color: 'white',
+            }}>Meme Creator</h1>
+            <div
+              className="inputsUser"
               style={{
                 display: "flex",
-                flexDirection: "column",
+                flexDirection: "row",
                 justifyContent: "center",
-                alignItems: "center",
-                // center the button
-                margin: 'auto',
-                // add top margin
-                marginTop: '20px',
-                // add padding
-                padding: '10px',
-                // add border
-                border: '1px solid black',
-                // add border radius
-                borderRadius: '5px',
-                // add background color
-                backgroundColor: 'lightblue',
-                // add cursor
-                cursor: 'pointer',
+                // add elements one next to the other
+                justifyContent: "center",
                 // margin bottom
                 marginBottom: '20px',
-              }}
-            >Generate another meme</button>
+              }}>
+              <button onClick={shuffleMemes} style={buttonStyle}>Shuffle</button>
+              {/* input file */}
+              <input type="file" id="input" multiple
+                style={buttonStyle} onChange={handleFileUpload} />
+            </div>
             <MemeCreator meme={memeDisplayed} />
-
           </> :
           <p>Loading...</p>
       }
--- src/components/MemeCreator.js
+++ src/components/MemeCreator.js
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+// import utils.js
+import exportAsImage from "../utils/exportAsImage.js";
 
 const MemeCreator = ({ meme }) => {
 
   const [topText, setTopText] = useState("");
   const [bottomText, setBottomText] = useState("");
+  const [fontSize, setFontSize] = useState("25px");
+  const exportRef = useRef();
+
+  const handleExport = () => {
+
+  };
+
 
   const handleTopTextChange = (e) => {
     setTopText(e.target.value);
@@ -13,6 +22,30 @@ const MemeCreator = ({ meme }) => {
     setBottomText(e.target.value);
   }
 
+  const handleFontSizeChange = (e) => {
+    setFontSize(`${parseInt(e.target.value)}px`);
+  }
+
+  const TextStyle = (
+    { position }) => {
+    const absolute = {
+      position: 'absolute',
+      left: '50%',
+      transform: 'translateX(-50%)',
+      margin: '0px',
+      fontFamily: 'Impact',
+      fontSize,
+      color: 'white',
+      textShadow: '2px 2px 0 black, -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black',
+      maxWidth: `${meme.width}px`,
+      wordBreak: 'break-all',
+    };
+
+    return position === 'top'
+      ? { ...absolute, top: '0', marginTop: '10px' }
+      : { ...absolute, bottom: '0', marginBottom: '10px' };
+  };
+
   return (
     <>
       <div style={{
@@ -21,8 +54,24 @@ const MemeCreator = ({ meme }) => {
         justifyContent: "center",
         alignItems: "center"
       }}>
-        <input type="text" placeholder="Top Text" onChange={handleTopTextChange} />
-        <input type="text" placeholder="Bottom Text" onChange={handleBottomTextChange} />
+        {/* add an input to clean all the other inputs */}
+        <input type="button" value="Clean" onClick={() => {
+          setTopText("");
+          setBottomText("");
+          setFontSize("25px");
+        }} />
+        <input type="text" placeholder="Top Text"
+          onChange={handleTopTextChange} />
+        <input type="text" placeholder="Bottom Text"
+          onChange={handleBottomTextChange} />
+        {/* input that stores the value into the variables fontSize to define the size of teh text by introducing a value*/}
+        <input type="number" placeholder={fontSize}
+          onChange={handleFontSizeChange}
+          // min value accepted is 10 max is 100
+          min="25" max="100"
+        />
+        <input type="button" value="Export"
+          onClick={() => exportAsImage(exportRef.current, "test.png")} />
       </div>
       <div style={{
         position: 'relative',
@@ -31,37 +80,25 @@ const MemeCreator = ({ meme }) => {
         // add top margin
         marginTop: '20px',
         // define max width and height
-        maxWidth: '700px',
+        maxWidth: '1200px',
         maxHeight: '700px',
         // align center
         textAlign: 'center',
         marginBottom: '20px',
-      }}>
-        <p style={{
-          position: 'absolute',
-          top: '0',
-          left: '50%',
-          transform: 'translateX(-50%)',
-          margin: '5px',
-          fontFamily: 'Impact',
-          fontSize: '200%',
-        }}>{topText.toUpperCase()}</p>
+      }} className="generatedMeme" ref={exportRef}>
+        <p style={TextStyle({ position: 'top' })}>
+          {topText.toUpperCase()}
+        </p>
         <img src={meme.url} alt="meme" style={{
           maxHeight: '700px',
           maxWidth: '700px',
           // center the image
-          margin: 'auto'
+          margin: 'auto',
         }}
         />
-        <p style={{
-          position: 'absolute',
-          bottom: '0',
-          left: '50%',
-          transform: 'translateX(-50%)',
-          margin: '5px',
-          fontFamily: 'Impact',
-          fontSize: '200%',
-        }}>{bottomText}</p>
+        <p style={TextStyle({ position: 'bottom' })}>
+          {bottomText.toUpperCase()}
+        </p>
       </div>
     </>
   );
