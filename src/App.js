0e4776f51595002c7f38a3a1dc230ca17f319ea2
JoanClaverol <41630699+JoanClaverol@users.noreply.github.com>
2023-03-12 20:09:48 +0100

    main updatated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { useState } from "react";
+import SwipeToConfirm from "./components/SwipeToConfirm";
 
 function App() {
+
+  const [startX, setStartX] = useState(null);
+  const [swiped, setSwiped] = useState(false);
+
   return (
     <div className="App">
-
+      <SwipeToConfirm />
     </div>
   );
 }
