a77f3ad5e14da9d5d8b6731fd58af7fa1cf0876f
JoanClaverol <41630699+JoanClaverol@users.noreply.github.com>
2023-03-12 20:12:23 +0100

    added clean verion template

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import { useState } from "react";
-import SwipeToConfirm from "./components/SwipeToConfirm";
 
 function App() {
 
-  const [startX, setStartX] = useState(null);
-  const [swiped, setSwiped] = useState(false);
-
   return (
     <div className="App">
-      <SwipeToConfirm />
     </div>
   );
 }
